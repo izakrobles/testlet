6c061120ec05906326f8f8f00d2a1720b551d1fd
Thomas Irvine <tom.irvine@slu.edu>
2023-03-01 14:50:47 -0600

    Account page changes

--- testlet/pages/account.js
+++ testlet/pages/account.js
@@ -1,8 +1,28 @@
 function Account() {
   return (
     <>
-      <title>Your Account</title>
-      <h2>Profile</h2>
+      <body>
+        <header>
+        <div class="user-profile">
+                  <h1><img align="right" src="https://datacenterresources.com/wp-content/uploads/2017/06/missing-profile-photo.png" alt=""/>Username<br/>First Last</h1>
+                  <p>&nbsp;</p>
+                  <p>Achievements Study Sets Expert Solutions Courses Classes</p>
+                  <p>&nbsp;</p>
+            </div>
+            <nav>
+                    <ul>
+                        <li><a href="#Recent-Activity"><b>Recent Activity</b></a></li>
+                        <li><a href="#Study"><b>Study</b></a></li>
+                        <li><a href="#Streaks"><b>Streaks</b></a></li>
+                        <li><a href="#Lifttime"><b>Liftime</b></a></li>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <li><a href="http://localhost:3000/LoginPages/forgotpwd"><b>Change Username</b></a></li>
+                        <li><a href="http://localhost:3000/LoginPages/forgotpwd"><b>Change Password</b></a></li>
+                      </ul>
+          </nav>
+        </header>
+      </body>
     </>
   );
 }
