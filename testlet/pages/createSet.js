b8e1363c3113e5e203965fc512ff131d17de82da
Izak Robles <izak.robles@slu.edu>
2023-03-01 19:39:22 -0600

    Create Sets Page Done

--- testlet/pages/createSet.js
+++ testlet/pages/createSet.js
@@ -3,21 +3,55 @@ import Flashcard from "./components/Flashcard";
 
 function CreateSet() {
   const [flashcards, setFlashcards] = useState([]);
-  const question = "What is a Blueberry?";
-  const answer = "A berry that is blue";
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
   const handleAddFlashcard = () => {
-    const newFlashcards = [...flashcards, { question, answer }];
+    const newFlashcards = [
+      ...flashcards,
+      { question: question, answer: answer },
+    ];
     setFlashcards(newFlashcards);
+    setAnswer("");
+    setQuestion("");
+  };
+
+  const handleSetQuestion = (event) => {
+    setQuestion(event.target.value);
+  };
+
+  const handleSetAnswer = (event) => {
+    setAnswer(event.target.value);
   };
 
   return (
     <>
       <title>Create a Set</title>
       <h1>Create New Set</h1>
-      <button className="create-set" onClick={handleAddFlashcard}>
-        Flashcard +
-      </button>
+      <div className="create-sets">
+        <form>
+          <label>Question:  </label>
+          <input
+            type="text"
+            className="question"
+            value={question}
+            onChange={handleSetQuestion}
+          ></input>
+        </form>
+        <form>
+          <label>Answer:  </label>
+          <input
+            type="text"
+            className="answer"
+            value={answer}
+            onChange={handleSetAnswer}
+          ></input>
+        </form>
+        </div>
+        <button className="create-set" onClick={handleAddFlashcard}>
+          Flashcard +
+        </button>
+
       {flashcards.map((flashcard, index) => (
         <Flashcard
           key={index}
