bc302a291eed686461f91def659e2b90c36bd269
Izak Robles <izak.robles@slu.edu>
2023-03-19 12:52:59 -0500

    Update _app.js

--- testlet/pages/_app.js
+++ testlet/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import Navbar from "./components/Navbar";
-import { Roboto } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
