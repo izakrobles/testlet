55f2f60bef354ee69e181a7915662f752234d574
Thomas Irvine <tom.irvine@slu.edu>
2023-04-04 15:55:07 -0500

    last draft for this branch, changing color to white

--- testlet/pages/account.js
+++ testlet/pages/account.js
@@ -64,10 +64,6 @@ const Account = () => {
 
           <Button onClick={handleLogout}>Logout</Button>
         </Container>
-
-        <footer className="foot bg-primary text-light text-center py-3">
-          <p className="mb-0">&copy; 2023 Testlet. All rights reserved.</p>
-        </footer>
       </div>
     </>
   );
