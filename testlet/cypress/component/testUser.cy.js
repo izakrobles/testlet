a814c2ba0c69debe6b88acf9d5923bed3f4f34e8
Thomas Irvine <tom.irvine@slu.edu>
2023-05-02 13:52:15 -0500

    Update testlet/cypress/component/testUser.cy.js

--- testlet/cypress/component/testUser.cy.js
+++ testlet/cypress/component/testUser.cy.js
@@ -1,15 +1,12 @@
-// Import the `noUser` component
 import NoUser from "../../pages/noUser";
 import React from "react";
 
 describe('No User Page', () => {
   beforeEach(() => {
-    // Load the page before each test
     cy.mount(<NoUser />);
   });
 
   it('displays user information', () => {
-    // Check if the user information is displayed correctly
     cy.contains('h2', 'JDough1');
     cy.contains('p', 'Jog Dough');
     cy.contains('p', 'Jog Dough Email');
